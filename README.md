230d97877d93612fa09c8edc3e1123a3d1bf69b2
alexzhang1030 <1642114555@qq.com>
2023-02-21 13:39:01 +0800

    chore: setup

---
+++ README.md
@@ -0,0 +1,19 @@
+# 可组合的 Vue
+
+- 心智模型
+  - composition vs options
+  - ref vs reactive
+  - 追踪更新的实现
+  - 数据流
+  - 逻辑与数据的关系
+  - 数据与视图的关系
+  - 封装组件考虑的因素
+  - 可组合的 Vue
+  - ...
+- 实践
+  - 组件
+  - 函数
+- Signals
+  - Build your own signals
+- SSR
+  - Build your own SSR framework
---
+++ app.config.ts
@@ -0,0 +1,18 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Composable Vue',
+    description: 'Vue 组合式 API 实践指南',
+    socials: {
+      github: 'alexzhang1030/composable-vue',
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      logo: true,
+      showLinkIcon: true,
+      exclude: []
+    },
+  }
+})
